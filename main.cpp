bf12e7497ebfbddcf33cde5de05e9bac6597a8ce
zsavery <zsavery998@gmail.com>
2020-04-05 03:39:02 -0500

    Add values to dataSet.txt and remove methids from main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,7 @@ using namespace std;
 int main()
 {
     binSearchTree b;
-    b.insert(20);
-    b.insert(10);
-    b.insert(5);
-    b.insert(15);
-    b.insert(40);
-    b.insert(45);
-    b.insert(30);
+    
 
-    cout << "Binary tree created: " << endl;
-    b.displayBinTree();
+    
 }
