50643f19a4a183041c753c16fe4b71ca34046cd0
zsavery <zsavery998@gmail.com>
2020-04-05 03:51:22 -0500

    Add file reading to maine from dataSet.txt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include "binaryTree.cpp"
+#include <fstream>
 using namespace std;
 
 int main()
 {
     binSearchTree b;
+
+    ifstream dataFile("dataSet.txt");
+    if(dataFile.is_open())
+    {
+        while(!dataFile.eof())
+        {
+            int data; 
+            dataFile >> data;
+            
+        }
+    }
+    dataFile.close();
     
 
     
